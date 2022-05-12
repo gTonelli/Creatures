ec17b42f7dbf73ae73c366fb81904b91dcc5241d
Graham Tonelli <gtonelli@outlook.com>
2022-05-12 10:48:31 -0500

    Completed creature generation and testing.

--- test_creature.py
+++ test_creature.py
@@ -1,5 +1,6 @@
 import unittest
 import creature
+import pybullet as p
 
 
 class TestCreature(unittest.TestCase):
@@ -18,6 +19,28 @@ class TestCreature(unittest.TestCase):
             links = c.get_flat_links()
             expanded_links = c.get_expanded_links()
             self.assertGreaterEqual(len(expanded_links), len(links))
+
+    def testToXMLNotNone(self):
+        c = creature.Creature(gene_count=2)
+        xml_str = c.to_xml()
+        self.assertIsNotNone(xml_str)
+
+    def testLoadXML(self):
+        c = creature.Creature(gene_count=2)
+        xml_str = c.to_xml()
+        with open('test.urdf', 'w') as f:
+            f.write(xml_str)
+        p.connect(p.DIRECT)
+
+        # p.connect(p.GUI)
+        # p.setPhysicsEngineParameter(enableFileCaching=0)
+        # p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+        # plane_shape = p.createCollisionShape(p.GEOM_PLANE)
+        # floor = p.createMultiBody(plane_shape, plane_shape)
+        # p.setGravity(0, 0, -10)
+
+        cid = p.loadURDF('test.urdf')
+        self.assertIsNotNone(cid)
 
 
 unittest.main()
